ad16e90b473a371ed78eb00ed8b7b6981fd7001b
support <support@WEMA-TRAINING-S>
2022-08-10 17:04:16 -0700

    Add project files.

---
+++ SCHOOLWEBSITE/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace SCHOOLWEBSITE
+{
+    internal class BsuWebsite
+    {
+        static void Main(string[] args)
+        {
+            Lecturers lecturer1 = new Lecturers();
+            //lecturer1.
+            Console.WriteLine("Enter the name of the lecturer: ");
+            //lecturer1. = Console.ReadLine();
+            
+        }
+    }
+}
---
+++ SCHOOLWEBSITE/Student.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCHOOLWEBSITE
+{
+    // Concept of abstraction
+    public abstract class Individuals
+    {
+        //encapsulation concept demonstrated.
+        //The Age variable is private to the Individuals class.
+        //To set the Age Variable, use getters and setters
+        private int Age; 
+        public int age
+        {
+            get { return Age; }
+            set { Age = value; }
+        }
+        public string Name { get; set; }
+        public string Id { get; set; }
+        public abstract string ValidateStatus();
+        public abstract void Register();
+
+    }
+
+    // Concept of inheritance
+    public class Students: Individuals
+    {
+        public override void Register()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ValidateResults()
+        {
+
+        }
+
+        public override string ValidateStatus()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Lecturers: Individuals // Concept of inheritance
+    {
+        
+        // Concept of polymorphism. A lecturer is also an individual.
+        // In this example, the lecturer is registering as an individual
+        public override void Register()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ValidateStatus()
+        {
+            throw new NotImplementedException();
+        }
+        public void GenerateResult() { }
+
+    }
+}
